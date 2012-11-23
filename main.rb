8ada0a4a2064470dec1120959381e0a80c6b0adf
Stacey Vetzal <stacey@vetzal.com>
2012-11-22 21:08:40 -0500

    yak shaving, tabs vs spaces

--- main.rb
+++ main.rb
@@ -5,8 +5,8 @@ square = ARGV[0] # Assume first command-line input is our argument
 square_root = square && Math.sqrt(square.to_i)
 
 unless square && square_root == square_root.to_i && square_root > 0
-	puts "You must provide an argument, and it must be a perfect square."
-	exit 0
+  puts "You must provide an argument, and it must be a perfect square."
+  exit 0
 end
 
 grid = Grid.new(square_root)
--- square.rb
+++ square.rb
@@ -3,38 +3,38 @@
 Coordinate = Struct.new(:x, :y)
 
 class Cursor
-	# Lookups would be nicer in a hash, but arrays guarantee order
-	DIRECTIONS = [ :right, :up, :left, :down ]
-	VECTORS = [ [1,0], [0,-1], [-1,0], [0,1] ]
+  # Lookups would be nicer in a hash, but arrays guarantee order
+  DIRECTIONS = [ :right, :up, :left, :down ]
+  VECTORS = [ [1,0], [0,-1], [-1,0], [0,1] ]
 
-	attr_accessor :direction, :coordinate
+  attr_accessor :direction, :coordinate
 
-	def initialize(coordinate)
-		self.direction = DIRECTIONS.first
-		self.coordinate = coordinate
-	end
+  def initialize(coordinate)
+  	self.direction = DIRECTIONS.first
+  	self.coordinate = coordinate
+  end
 
-	def next_coordinate
-		move = VECTORS[DIRECTIONS.index(self.direction)]
-		Coordinate.new(coordinate.x + move[0], coordinate.y + move[1])
-	end
+  def next_coordinate
+  	move = VECTORS[DIRECTIONS.index(self.direction)]
+  	Coordinate.new(coordinate.x + move[0], coordinate.y + move[1])
+  end
 
-	def turned_coordinate
-		move = VECTORS[DIRECTIONS.index(self.next_direction)]
-		Coordinate.new(coordinate.x + move[0], coordinate.y + move[1])
-	end
+  def turned_coordinate
+  	move = VECTORS[DIRECTIONS.index(self.next_direction)]
+  	Coordinate.new(coordinate.x + move[0], coordinate.y + move[1])
+  end
 
-	def rotate_direction
-		self.direction = next_direction
-	end
+  def rotate_direction
+  	self.direction = next_direction
+  end
 
-	def next_direction
-		DIRECTIONS[DIRECTIONS.index(self.direction) + 1] || DIRECTIONS.first
-	end
+  def next_direction
+  	DIRECTIONS[DIRECTIONS.index(self.direction) + 1] || DIRECTIONS.first
+  end
 
-	def advance
-		self.coordinate = next_coordinate
-	end
+  def advance
+  	self.coordinate = next_coordinate
+  end
 end
 
 class Grid 
@@ -47,56 +47,56 @@ class Grid
     self.counter = 1
     self.data_storage = Array.new(size)
     (1..size).each do |number|
-    	self.data_storage[number-1] = Array.new(size) { 0 }
+      self.data_storage[number-1] = Array.new(size) { 0 }
     end 
   end
 
   def calculate_origin
-  	if size % 2 == 0
-			x = (size / 2) - 1
-			y = size / 2
-	  else
-		  x = size / 2
-		  y = size / 2
-	  end
-  	self.origin = Coordinate.new(x, y)
+    if size % 2 == 0
+  		x = (size / 2) - 1
+  		y = size / 2
+    else
+  	  x = size / 2
+  	  y = size / 2
+    end
+    self.origin = Coordinate.new(x, y)
   end
 
   def data(coordinate)
-  	self.data_storage[coordinate.y][coordinate.x]
+    self.data_storage[coordinate.y][coordinate.x]
   end
 
   def cursor_at_edge
-  	self.cursor.direction == :right && self.cursor.coordinate.x == size-1 \
-  	|| self.cursor.direction == :up && self.cursor.coordinate.y == 0 \
-  	|| self.cursor.direction == :down && self.cursor.coordinate.y == size-1 \
-  	|| self.cursor.direction == :left && self.cursor.coordinate.x == 0
+    self.cursor.direction == :right && self.cursor.coordinate.x == size-1 \
+    || self.cursor.direction == :up && self.cursor.coordinate.y == 0 \
+    || self.cursor.direction == :down && self.cursor.coordinate.y == size-1 \
+    || self.cursor.direction == :left && self.cursor.coordinate.x == 0
   end
 
   def cursor_can_turn
-  	self.data(self.cursor.turned_coordinate) == 0
+    self.data(self.cursor.turned_coordinate) == 0
   end
 
   def advance_cursor
-  	self.data_storage[cursor.coordinate.y][cursor.coordinate.x] = self.counter
-  	self.counter += 1
-  	self.cursor.advance
-  	self.cursor.rotate_direction if cursor_at_edge || cursor_can_turn
+    self.data_storage[cursor.coordinate.y][cursor.coordinate.x] = self.counter
+    self.counter += 1
+    self.cursor.advance
+    self.cursor.rotate_direction if cursor_at_edge || cursor_can_turn
   end
 
   def finished
-  	self.counter > self.size * self.size
+    self.counter > self.size * self.size
   end
 
   def fill
-  	until self.finished
-  		self.advance_cursor
-  	end
+    until self.finished
+    	self.advance_cursor
+    end
   end
 
   def print_matrix
-  	column_width = (self.size*self.size).to_s.length + 1
-  	format = "%#{column_width}d"
+    column_width = (self.size*self.size).to_s.length + 1
+    format = "%#{column_width}d"
     output = self.data_storage.collect do |row|
       row.collect { |col| format % col }.join
     end.join "\n"
